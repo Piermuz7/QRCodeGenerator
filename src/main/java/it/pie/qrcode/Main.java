6c0c40a2359f976437611b5aeb9f76b3786d5d90
Piermuz <piermichele.rosati@gmail.com>
2022-01-20 15:57:45 +0100

    Changed name in calculator

--- src/main/java/it/pie/qrcode/Main.java
+++ src/main/java/it/pie/qrcode/Main.java
@@ -7,12 +7,12 @@ public class Main {
     public static final File dst_path = new File("C:\\Users\\Piermuz\\Desktop\\QRCodes\\");
 
     public static void main(String[] args) {
-        QrCodeCalculator controller = new QrCodeCalculator();
-        controller.createQRCode("This is the first generated QRCode",dst_path,"0","PNG");
-        controller.createQRCode("This is the second generated QRCode",dst_path,"1","PNG");
-        controller.createQRCode("This is the third generated QRCode",dst_path,"2","PNG");
+        QrCodeCalculator calculator = new QrCodeCalculator();
+        calculator.createQRCode("This is the first generated QRCode",dst_path,"0","PNG");
+        calculator.createQRCode("This is the second generated QRCode",dst_path,"1","PNG");
+        calculator.createQRCode("This is the third generated QRCode",dst_path,"2","PNG");
         for(int i = 0; i < 3; i++)
-            System.out.println("QRCode content: "+controller.readQRCode(dst_path.getAbsolutePath()+"\\"+i+".PNG"));
+            System.out.println("QRCode content: "+calculator.readQRCode(dst_path.getAbsolutePath()+"\\"+i+".PNG"));
     }
 
 }
